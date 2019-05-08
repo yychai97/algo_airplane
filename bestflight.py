fd41b33d3d710f60b4a688206abc6047e6aaa6b2
yychai97 <49704239+yychai97@users.noreply.github.com>
2019-05-08 15:26:14 +0800

    Update

--- bestflight.py
+++ bestflight.py
@@ -1,8 +1,54 @@
+from _ast import keyword
+
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
+import sys
+import polyline
+import googlemaps
+from datetime import datetime
+
+
+##Djisktra ALgo for calc
+class Graph():
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)] for row in range(vertices)]
+
+    def printSolution(self, dist):
+        print("Vertex tDistance from Source")
+        for node in range(self.V):
+            print(node, "t", dist[node])
+
+    def minDistance(self, dist, sptSet):
+        min = sys.maxsize
+
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+
+        return min_index
+
+    def djikstra(self, src):
+
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0
+        sptSet = [False] * (self.V)
+
+        for cout in range(self.V):
+            u = self.minDistance(dist, sptSet)
+            sptSet[u] = True
+
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]:
+                    dist[v] = dist[u] + self.graph[u][v]
+        self.printSolution(dist)
+
+
 
 plotly.tools.set_credentials_file(username = 'yychai97', api_key = 'OWIMPYbRvRbxNupsoiWe')
 geolocator = Nominatim(user_agent = "wia2005")
@@ -28,3 +74,21 @@ trace1 = go.Scatter(
 data = [trace0, trace1]
 
 py.plot(data, filename = 'basic-line', auto_open=True)
+
+g = Graph(9)
+g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+           [4, 0, 8, 0, 0, 0, 0, 11, 0],
+           [0, 8, 0, 7, 0, 4, 0, 0, 2],
+           [0, 0, 7, 0, 9, 14, 0, 0, 0],
+           [0, 0, 0, 9, 0, 10, 0, 0, 0],
+           [0, 0, 4, 14, 10, 0, 2, 0, 0],
+           [0, 0, 0, 0, 0, 2, 0, 1, 6],
+           [8, 11, 0, 0, 0, 0, 1, 0, 7],
+           [0, 0, 2, 0, 0, 0, 6, 7, 0]
+          ]
+
+g.djikstra(0)
+
+####################
+
+gmaps = googlemaps.Client(key='AIzaSyAKeF3vJdrKjN7YHsDKAfrOFjP5wLxaSo8')
