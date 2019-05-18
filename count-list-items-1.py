a086cb9d233bc086dfdb5ccb5cdebcffaca7a664
nelsonteo <50214024+nelsonteo@users.noreply.github.com>
2019-05-18 22:08:30 +0800

    Delete count-list-items-1.py

--- count-list-items-1.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-def word_count(filename):
-    with open(filename) as f:
-        return Counter(f.read().split())
-
-counter = word_count('//insert text file')
-for i in counter:
-    print(i, ':', counter[i])
