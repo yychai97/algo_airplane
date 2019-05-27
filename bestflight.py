c2c7b632d9683f4f31f6bb4e0e7aff671222b681
yychai97 <49704239+yychai97@users.noreply.github.com>
2019-05-27 09:57:12 +0800

    update missing news braz

--- bestflight.py
+++ bestflight.py
@@ -124,8 +124,7 @@ class Graph:
             if distances[current_vertex] == inf:
                 break
             for neighbour, cost in self.neighbours[current_vertex]:
-                newWeight = getWeight(neighbour)
-                alternative_route = distances[current_vertex] + cost + newWeight
+                alternative_route = distances[current_vertex] + cost
                 if alternative_route < distances[neighbour]:
                     distances[neighbour] = alternative_route
                     previous_vertices[neighbour] = current_vertex
@@ -247,66 +246,66 @@ graph = Graph([
     ("braz", "usa", geodesic(brazcoordinate, usacoordinate).kilometers)])
 
 graph_weighted = Graph([
-    ("kul", "thai", geodesic(kulcoordinate, thaicoordinate).kilometers),
-    ("kul", "hk", geodesic(kulcoordinate, hkcoordinate).kilometers),
-    ("kul", "sgp", geodesic(kulcoordinate, sgpcoordinate).kilometers),
-    ("thai", "nz", geodesic(thaicoordinate, nzcoordinate).kilometers),
-    ("thai", "ger", geodesic(thaicoordinate, gercoordinate).kilometers),
-    ("thai", "haw", geodesic(thaicoordinate, hawcoordinate).kilometers),
-    ("thai", "hk", geodesic(thaicoordinate, hkcoordinate).kilometers),
-    ("usa", "braz", geodesic(usacoordinate, brazcoordinate).kilometers),
-    ("hk", "thai", geodesic(hkcoordinate, thaicoordinate).kilometers),
-    ("hk", "nz", geodesic(hkcoordinate, nzcoordinate).kilometers),
-    ("hk", "ger", geodesic(hkcoordinate, gercoordinate).kilometers),
-    ("hk", "haw", geodesic(hkcoordinate, hawcoordinate).kilometers),
-    ("hk", "sgp", geodesic(hkcoordinate, sgpcoordinate).kilometers),
-    ("sgp", "hk", geodesic(sgpcoordinate, hkcoordinate).kilometers),
-    ("sgp", "nz", geodesic(sgpcoordinate, nzcoordinate).kilometers),
-    ("sgp", "ger", geodesic(sgpcoordinate, gercoordinate).kilometers),
-    ("sgp", "haw", geodesic(sgpcoordinate, hawcoordinate).kilometers),
-    ("nz", "jpn", geodesic(nzcoordinate, jpncoordinate).kilometers),
-    ("nz", "usa", geodesic(nzcoordinate, usacoordinate).kilometers),
-    ("nz", "ger", geodesic(nzcoordinate, gercoordinate).kilometers),
-    ("nz", "thai", geodesic(nzcoordinate, thaicoordinate).kilometers),
-    ("nz", "hk", geodesic(nzcoordinate, hkcoordinate).kilometers),
-    ("nz", "sgp", geodesic(nzcoordinate, sgpcoordinate).kilometers),
-    ("ger", "jpn", geodesic(gercoordinate, jpncoordinate).kilometers),
-    ("ger", "usa", geodesic(gercoordinate, usacoordinate).kilometers),
-    ("ger", "haw", geodesic(gercoordinate, hawcoordinate).kilometers),
-    ("ger", "thai", geodesic(gercoordinate, thaicoordinate).kilometers),
-    ("ger", "hk", geodesic(gercoordinate, hkcoordinate).kilometers),
-    ("ger", "sgp", geodesic(gercoordinate, sgpcoordinate).kilometers),
-    ("ger", "nz", geodesic(gercoordinate, nzcoordinate).kilometers),
-    ("haw", "jpn", geodesic(hawcoordinate, jpncoordinate).kilometers),
-    ("haw", "usa", geodesic(hawcoordinate, usacoordinate).kilometers),
-    ("haw", "thai", geodesic(hawcoordinate, thaicoordinate).kilometers),
-    ("haw", "hk", geodesic(hawcoordinate, hkcoordinate).kilometers),
-    ("haw", "sgp", geodesic(hawcoordinate, sgpcoordinate).kilometers),
-    ("haw", "ger", geodesic(hawcoordinate, gercoordinate).kilometers),
-    ("jpn", "aus", geodesic(jpncoordinate, auscoordinate).kilometers),
-    ("jpn", "uk", geodesic(jpncoordinate, ukcoordinate).kilometers),
-    ("jpn", "braz", geodesic(jpncoordinate, brazcoordinate).kilometers),
-    ("jpn", "usa", geodesic(jpncoordinate, usacoordinate).kilometers),
-    ("jpn", "nz", geodesic(jpncoordinate, nzcoordinate).kilometers),
-    ("jpn", "ger", geodesic(jpncoordinate, gercoordinate).kilometers),
-    ("jpn", "haw", geodesic(jpncoordinate, hawcoordinate).kilometers),
-    ("usa", "aus", geodesic(usacoordinate, auscoordinate).kilometers),
-    ("usa", "uk", geodesic(usacoordinate, ukcoordinate).kilometers),
-    ("usa", "braz", geodesic(usacoordinate, brazcoordinate).kilometers),
-    ("usa", "nz", geodesic(usacoordinate, nzcoordinate).kilometers),
-    ("usa", "ger", geodesic(usacoordinate, gercoordinate).kilometers),
-    ("usa", "haw", geodesic(usacoordinate, hawcoordinate).kilometers),
-    ("usa", "jpn", geodesic(usacoordinate, jpncoordinate).kilometers),
-    ("aus", "uk", geodesic(auscoordinate, ukcoordinate).kilometers),
-    ("aus", "jpn", geodesic(auscoordinate, jpncoordinate).kilometers),
-    ("aus", "usa", geodesic(auscoordinate, usacoordinate).kilometers),
-    ("uk", "jpn", geodesic(ukcoordinate, jpncoordinate).kilometers),
-    ("uk", "usa", geodesic(ukcoordinate, usacoordinate).kilometers),
-    ("uk", "aus", geodesic(ukcoordinate, auscoordinate).kilometers),
-    ("uk", "braz", geodesic(ukcoordinate, brazcoordinate).kilometers),
-    ("braz", "uk", geodesic(brazcoordinate, ukcoordinate).kilometers),
-    ("braz", "jpn", geodesic(brazcoordinate, jpncoordinate).kilometers),
-    ("braz", "usa", geodesic(brazcoordinate, usacoordinate).kilometers)])
+    ("kul", "thai", geodesic(kulcoordinate, thaicoordinate).kilometers + country_list["thai"].count_sentiment()),
+    ("kul", "hk", geodesic(kulcoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
+    ("kul", "sgp", geodesic(kulcoordinate, sgpcoordinate).kilometers + country_list["sgp"].count_sentiment()),
+    ("thai", "nz", geodesic(thaicoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()   ),
+    ("thai", "ger", geodesic(thaicoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
+    ("thai", "haw", geodesic(thaicoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
+    ("thai", "hk", geodesic(thaicoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
+    ("usa", "braz", geodesic(usacoordinate, brazcoordinate).kilometers + country_list["braz"].count_sentiment()),
+    ("hk", "thai", geodesic(hkcoordinate, thaicoordinate).kilometers + country_list["thai"].count_sentiment()),
+    ("hk", "nz", geodesic(hkcoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()),
+    ("hk", "ger", geodesic(hkcoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
+    ("hk", "haw", geodesic(hkcoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
+    ("hk", "sgp", geodesic(hkcoordinate, sgpcoordinate).kilometers + country_list["sgp"].count_sentiment()),
+    ("sgp", "hk", geodesic(sgpcoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
+    ("sgp", "nz", geodesic(sgpcoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()),
+    ("sgp", "ger", geodesic(sgpcoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
+    ("sgp", "haw", geodesic(sgpcoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
+    ("nz", "jpn", geodesic(nzcoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
+    ("nz", "usa", geodesic(nzcoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
+    ("nz", "ger", geodesic(nzcoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
+    ("nz", "thai", geodesic(nzcoordinate, thaicoordinate).kilometers + country_list["thai"].count_sentiment()),
+    ("nz", "hk", geodesic(nzcoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
+    ("nz", "sgp", geodesic(nzcoordinate, sgpcoordinate).kilometers + country_list["sgp"].count_sentiment()),
+    ("ger", "jpn", geodesic(gercoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
+    ("ger", "usa", geodesic(gercoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
+    ("ger", "haw", geodesic(gercoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
+    ("ger", "thai", geodesic(gercoordinate, thaicoordinate).kilometers + country_list["thai"].count_sentiment()),
+    ("ger", "hk", geodesic(gercoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
+    ("ger", "sgp", geodesic(gercoordinate, sgpcoordinate).kilometers + country_list["sgp"].count_sentiment()),
+    ("ger", "nz", geodesic(gercoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()),
+    ("haw", "jpn", geodesic(hawcoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
+    ("haw", "usa", geodesic(hawcoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
+    ("haw", "thai", geodesic(hawcoordinate, thaicoordinate).kilometers + country_list["thai"].count_sentiment()),
+    ("haw", "hk", geodesic(hawcoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
+    ("haw", "sgp", geodesic(hawcoordinate, sgpcoordinate).kilometers + country_list["sgp"].count_sentiment()),
+    ("haw", "ger", geodesic(hawcoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
+    ("jpn", "aus", geodesic(jpncoordinate, auscoordinate).kilometers + country_list["aus"].count_sentiment()),
+    ("jpn", "uk", geodesic(jpncoordinate, ukcoordinate).kilometers + country_list["uk"].count_sentiment()),
+    ("jpn", "braz", geodesic(jpncoordinate, brazcoordinate).kilometers + country_list["braz"].count_sentiment()),
+    ("jpn", "usa", geodesic(jpncoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
+    ("jpn", "nz", geodesic(jpncoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()),
+    ("jpn", "ger", geodesic(jpncoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
+    ("jpn", "haw", geodesic(jpncoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
+    ("usa", "aus", geodesic(usacoordinate, auscoordinate).kilometers + country_list["aus"].count_sentiment()),
+    ("usa", "uk", geodesic(usacoordinate, ukcoordinate).kilometers + country_list["uk"].count_sentiment()),
+    ("usa", "braz", geodesic(usacoordinate, brazcoordinate).kilometers + country_list["braz"].count_sentiment()),
+    ("usa", "nz", geodesic(usacoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()),
+    ("usa", "ger", geodesic(usacoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
+    ("usa", "haw", geodesic(usacoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
+    ("usa", "jpn", geodesic(usacoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
+    ("aus", "uk", geodesic(auscoordinate, ukcoordinate).kilometers + country_list["uk"].count_sentiment()),
+    ("aus", "jpn", geodesic(auscoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
+    ("aus", "usa", geodesic(auscoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
+    ("uk", "jpn", geodesic(ukcoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
+    ("uk", "usa", geodesic(ukcoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
+    ("uk", "aus", geodesic(ukcoordinate, auscoordinate).kilometers + country_list["aus"].count_sentiment()),
+    ("uk", "braz", geodesic(ukcoordinate, brazcoordinate).kilometers + country_list["braz"].count_sentiment()),
+    ("braz", "uk", geodesic(brazcoordinate, ukcoordinate).kilometers + country_list["uk"].count_sentiment()),
+    ("braz", "jpn", geodesic(brazcoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
+    ("braz", "usa", geodesic(brazcoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment())])
 
 
 
@@ -322,9 +321,9 @@ def callHEREMAPS(locationlist):
 ########################################################################################################################
 ##WEIGHTING POLITICAL SENTIMENT INTO DATA
 
-def getWeight(neighborCode):
-    weight = country_list[neighborCode].count_sentiment()
-    return weight
+#def getWeight(neighborCode):
+#   weight = country_list[neighborCode].count_sentiment()
+ #   return weight
 
 
 ########################################################################################################################
