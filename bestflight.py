f96f5524c616a29dfb0f7aff432be2948efe819e
lee1409 <leerun1409@gmail.com>
2019-05-27 15:29:00 +0800

    Remove 3 news source

--- bestflight.py
+++ bestflight.py
@@ -13,23 +13,22 @@ import ssl
 import certifi
 import copy
 
-
-
 ##DIJSKTRA ALGORITHM for calculation
 # we'll use infinity as a default distance to nodes.
 inf = float('inf')
 Edge = namedtuple('Edge', 'start, end, cost')
 
-
 weight_list = []
 
+
 def make_edge(start, end, cost=1):
-  return Edge(start, end, cost)
+    return Edge(start, end, cost)
 
 
 class Graph:
-    def __init__(self, edges):
+    def __init__(self, edges, country_list):
         # let's check that the data is right
+        self.country_list = country_list
         wrong_edges = [i for i in edges if len(i) not in [2, 3]]
         if wrong_edges:
             raise ValueError('Wrong edges data: {}'.format(wrong_edges))
@@ -107,7 +106,6 @@ class Graph:
         callHEREMAPS(mappath)
         return path
 
-
     def dijkstra_with_weight(self, source, dest):
         assert source in self.vertices, 'Such source node doesn\'t exist'
         distances = {vertex: inf for vertex in self.vertices}
@@ -124,7 +122,7 @@ class Graph:
             if distances[current_vertex] == inf:
                 break
             for neighbour, cost in self.neighbours[current_vertex]:
-                alternative_route = distances[current_vertex] + cost
+                alternative_route = self.country_list[neighbour].count_sentiment() * distances[current_vertex] + cost
                 if alternative_route < distances[neighbour]:
                     distances[neighbour] = alternative_route
                     previous_vertices[neighbour] = current_vertex
@@ -136,15 +134,12 @@ class Graph:
         if path:
             path.appendleft(current_vertex)
         mappath = path.copy()
-        callHEREMAPS(mappath)
         return path
-
-
 
 
 ##---------------------------------------------------------------------------------------------------------##
 ##OBTAINING COORDINATES FOR CITIES
-plotly.tools.set_credentials_file(username = 'yychai97', api_key = 'TsWmwKFkn3hd8MMIDVEA')
+plotly.tools.set_credentials_file(username='yychai97', api_key='TsWmwKFkn3hd8MMIDVEA')
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
@@ -175,7 +170,6 @@ brazcoordinate = (braz.latitude, braz.longitude)
 hawcoordinate = (haw.latitude, haw.longitude)
 hkcoordinate = (hk.latitude, hk.longitude)
 sgpcoordinate = (sgp.latitude, sgp.longitude)
-
 
 country_list = main()
 
@@ -243,73 +237,8 @@ graph = Graph([
     ("uk", "braz", geodesic(ukcoordinate, brazcoordinate).kilometers),
     ("braz", "uk", geodesic(brazcoordinate, ukcoordinate).kilometers),
     ("braz", "jpn", geodesic(brazcoordinate, jpncoordinate).kilometers),
-    ("braz", "usa", geodesic(brazcoordinate, usacoordinate).kilometers)])
+    ("braz", "usa", geodesic(brazcoordinate, usacoordinate).kilometers)], country_list=country_list)
 
-graph_weighted = Graph([
-    ("kul", "thai", geodesic(kulcoordinate, thaicoordinate).kilometers + country_list["thai"].count_sentiment()),
-    ("kul", "hk", geodesic(kulcoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
-    ("kul", "sgp", geodesic(kulcoordinate, sgpcoordinate).kilometers + country_list["sgp"].count_sentiment()),
-    ("thai", "nz", geodesic(thaicoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()   ),
-    ("thai", "ger", geodesic(thaicoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
-    ("thai", "haw", geodesic(thaicoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
-    ("thai", "hk", geodesic(thaicoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
-    ("usa", "braz", geodesic(usacoordinate, brazcoordinate).kilometers + country_list["braz"].count_sentiment()),
-    ("hk", "thai", geodesic(hkcoordinate, thaicoordinate).kilometers + country_list["thai"].count_sentiment()),
-    ("hk", "nz", geodesic(hkcoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()),
-    ("hk", "ger", geodesic(hkcoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
-    ("hk", "haw", geodesic(hkcoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
-    ("hk", "sgp", geodesic(hkcoordinate, sgpcoordinate).kilometers + country_list["sgp"].count_sentiment()),
-    ("sgp", "hk", geodesic(sgpcoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
-    ("sgp", "nz", geodesic(sgpcoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()),
-    ("sgp", "ger", geodesic(sgpcoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
-    ("sgp", "haw", geodesic(sgpcoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
-    ("nz", "jpn", geodesic(nzcoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
-    ("nz", "usa", geodesic(nzcoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
-    ("nz", "ger", geodesic(nzcoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
-    ("nz", "thai", geodesic(nzcoordinate, thaicoordinate).kilometers + country_list["thai"].count_sentiment()),
-    ("nz", "hk", geodesic(nzcoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
-    ("nz", "sgp", geodesic(nzcoordinate, sgpcoordinate).kilometers + country_list["sgp"].count_sentiment()),
-    ("ger", "jpn", geodesic(gercoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
-    ("ger", "usa", geodesic(gercoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
-    ("ger", "haw", geodesic(gercoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
-    ("ger", "thai", geodesic(gercoordinate, thaicoordinate).kilometers + country_list["thai"].count_sentiment()),
-    ("ger", "hk", geodesic(gercoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
-    ("ger", "sgp", geodesic(gercoordinate, sgpcoordinate).kilometers + country_list["sgp"].count_sentiment()),
-    ("ger", "nz", geodesic(gercoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()),
-    ("haw", "jpn", geodesic(hawcoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
-    ("haw", "usa", geodesic(hawcoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
-    ("haw", "thai", geodesic(hawcoordinate, thaicoordinate).kilometers + country_list["thai"].count_sentiment()),
-    ("haw", "hk", geodesic(hawcoordinate, hkcoordinate).kilometers + country_list["hk"].count_sentiment()),
-    ("haw", "sgp", geodesic(hawcoordinate, sgpcoordinate).kilometers + country_list["sgp"].count_sentiment()),
-    ("haw", "ger", geodesic(hawcoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
-    ("jpn", "aus", geodesic(jpncoordinate, auscoordinate).kilometers + country_list["aus"].count_sentiment()),
-    ("jpn", "uk", geodesic(jpncoordinate, ukcoordinate).kilometers + country_list["uk"].count_sentiment()),
-    ("jpn", "braz", geodesic(jpncoordinate, brazcoordinate).kilometers + country_list["braz"].count_sentiment()),
-    ("jpn", "usa", geodesic(jpncoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
-    ("jpn", "nz", geodesic(jpncoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()),
-    ("jpn", "ger", geodesic(jpncoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
-    ("jpn", "haw", geodesic(jpncoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
-    ("usa", "aus", geodesic(usacoordinate, auscoordinate).kilometers + country_list["aus"].count_sentiment()),
-    ("usa", "uk", geodesic(usacoordinate, ukcoordinate).kilometers + country_list["uk"].count_sentiment()),
-    ("usa", "braz", geodesic(usacoordinate, brazcoordinate).kilometers + country_list["braz"].count_sentiment()),
-    ("usa", "nz", geodesic(usacoordinate, nzcoordinate).kilometers + country_list["nz"].count_sentiment()),
-    ("usa", "ger", geodesic(usacoordinate, gercoordinate).kilometers + country_list["ger"].count_sentiment()),
-    ("usa", "haw", geodesic(usacoordinate, hawcoordinate).kilometers + country_list["haw"].count_sentiment()),
-    ("usa", "jpn", geodesic(usacoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
-    ("aus", "uk", geodesic(auscoordinate, ukcoordinate).kilometers + country_list["uk"].count_sentiment()),
-    ("aus", "jpn", geodesic(auscoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
-    ("aus", "usa", geodesic(auscoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
-    ("uk", "jpn", geodesic(ukcoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
-    ("uk", "usa", geodesic(ukcoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment()),
-    ("uk", "aus", geodesic(ukcoordinate, auscoordinate).kilometers + country_list["aus"].count_sentiment()),
-    ("uk", "braz", geodesic(ukcoordinate, brazcoordinate).kilometers + country_list["braz"].count_sentiment()),
-    ("braz", "uk", geodesic(brazcoordinate, ukcoordinate).kilometers + country_list["uk"].count_sentiment()),
-    ("braz", "jpn", geodesic(brazcoordinate, jpncoordinate).kilometers + country_list["jpn"].count_sentiment()),
-    ("braz", "usa", geodesic(brazcoordinate, usacoordinate).kilometers + country_list["usa"].count_sentiment())])
-
-
-
-########################################################################################################################
 ##MAPPING LINES AND DISTANCE USING HERE MAPS
 
 def callHEREMAPS(locationlist):
@@ -319,18 +248,7 @@ def callHEREMAPS(locationlist):
 
 
 ########################################################################################################################
-##WEIGHTING POLITICAL SENTIMENT INTO DATA
-
-#def getWeight(neighborCode):
-#   weight = country_list[neighborCode].count_sentiment()
- #   return weight
-
-
-########################################################################################################################
 ##PROBABILITY OF RANDOM ROUTES
-
-
-
 
 
 ########################################################################################################################
@@ -343,34 +261,14 @@ def main():
     print(graph.dijkstra("kul", "jpn"))
     print(graph.dijkstra("kul", "aus"))
     print("After adding weight of political sentiment, list of destinations: ")
-    print(graph_weighted.dijkstra_with_weight("kul", "usa"))
-    print(graph_weighted.dijkstra_with_weight("kul", "ger"))
-    print(graph_weighted.dijkstra_with_weight("kul", "uk"))
-    print(graph_weighted.dijkstra_with_weight("kul", "braz"))
-    print(graph_weighted.dijkstra_with_weight("kul", "jpn"))
-    print(graph_weighted.dijkstra_with_weight("kul", "aus"))
+    print(graph.dijkstra_with_weight("kul", "usa"))
+    print(graph.dijkstra_with_weight("kul", "ger"))
+    print(graph.dijkstra_with_weight("kul", "uk"))
+    print(graph.dijkstra_with_weight("kul", "braz"))
+    print(graph.dijkstra_with_weight("kul", "jpn"))
+    print(graph.dijkstra_with_weight("kul", "aus"))
 
 
-
-""" now = time.time()
-    country_list = {}
-    for i in os.listdir("news"):
-        # Read everything
-        country = i[:-6]
-        newspaper_list = country_list.setdefault(country, [])
-        f = open(os.path.join("news", i), encoding='ISO-8859-1')
-        news = Newspaper(country, f.read())
-        f.close()
-        news.generate_word_stop()
-        news.generate_sentiment()
-        newspaper_list.append(news)
-        country_list[country] = newspaper_list
-
-    plot_count(country_list)
-    for name, newspaper_list in country_list.items():
-        plot_sentiment(newspaper_list, name) """
-
-  #  return country_list
 #  print(time.time() - now)
 if __name__ == "__main__":
     main()
